917ab5013ee9100804b5aa7434d9f3c579dcb539
ai.flyingwheel <catslinc0@gmail.com>
2024-06-22 00:52:47 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const ENABLE_PROTECT = !isUndefined(process.env.ACCESS_PASSWORD) && process.env.
 
 const APP_NAME = 'Talk with Telll'
 const APP_DEFAULT_TITLE = 'Talk with Telll'
-const APP_TITLE_TEMPLATE = '%s - PWA App'
+const APP_TITLE_TEMPLATE = '%s - AI-Powerd SAS'
 const APP_DESCRIPTION =
   'Deploy your private Gemini application for free with one click, supporting Gemini 1.5 Pro, Gemini 1.5 Flash, Gemini Pro and Gemini Pro Vision models. 一键免费部署您的私人 Gemini 应用, 支持 Gemini 1.5 Pro、Gemini 1.5 Flash、Gemini Pro 和 Gemini Pro Vision 模型。'
 
